22b9cc95fae740977cf026490890214561423316
HuyenAnh33 <40015224+HuyenAnh33@users.noreply.github.com>
2018-06-19 16:53:34 +0700

    bai 1

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -4,26 +4,70 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class GameCanvas extends JPanel {
 
     BufferedImage starImage;//ten bien ro rang, de lam gi
+    BufferedImage enemyImage;
+    BufferedImage playerImage;
+
+
+    //BackBuffered lat hinh anh
+    BufferedImage backBuffered;
+    Graphics graphics;//co ve nay se ve het len buffered
+
+    Random random = new Random();
+
+    public int positionXStar = 400;
+    int positionYStar = 200; //vi tri
+
+
+    public int positionXEnemy = 100;
+    public int positionYEnemy = 200;
+
+
+    public int positionXPlayer = 200;
+    public int positionYPlayer = 300;
+
+    public int moveXEnemy = 8;
+    public int moveYEnemy = 8;
+
+
     public GameCanvas(){
         this.setSize(1024,600);
 
+        this.backBuffered = new BufferedImage(1024,600,BufferedImage.TYPE_4BYTE_ABGR);
+        this.graphics = this.backBuffered.getGraphics();//gan vao backbuffered
+
         try {//thu load anh neu loi se nhay xg catch
             this.starImage = ImageIO.read(new File("resources/images/star.png")); //dua duong dan tuong doi
+
+            this.enemyImage = ImageIO.read(new File("resources/images/circle.png"));
+            this.playerImage = ImageIO.read(new File ("resources/images/circle.png") );
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         this.setVisible(true);
     }
-
+    //paintComponent chi la de ve va lat hinh anh
     @Override
-    protected void paintComponent(Graphics g) { //ve tat ca mmoi thu o trong nay thoi (graphic la ve)
+    protected void paintComponent(Graphics g) { //ve tat ca mmoi thu o trong nay thoi (graphic la ve hien thi g)
        // Color = new Color();//chon bang mau
-        g.setColor(Color.BLACK);//tao nen
-        g.fillRect(0,0,1024,600);// but ve hinh chu nhat
-        g.drawImage(this.starImage, 400,200,null);//bat dau ve buc anh(truyen anh,toa do)
+//
+
+        g.drawImage(this.backBuffered, 0,0,null);
+    }
+
+
+    public void renderAll(){
+        this.graphics.setColor(Color.BLACK);//tao nen
+        this.graphics.fillRect(0,0,1024,600);// but ve hinh chu nhat
+        this.graphics.drawImage(this.starImage, this.positionXStar, this.positionYStar,5,5, null);
+        this.graphics.drawImage(this.enemyImage,this.positionXEnemy,this.positionYEnemy,20,20,null);
+        this.graphics.drawImage(this.playerImage,this.positionXPlayer,this.positionYPlayer,10,10,null);
+
+        this.repaint();// be tu gamewindow va this chinh la gamecavas
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,17 +1,131 @@
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class GameWindow extends JFrame {
 
     GameCanvas gameCanvas;// dung de ve tat ca moi thu len
+    long lastTime = 0;
+    Random random = new Random();
 
-    public  GameWindow(){
-        this.setSize(1024,600);
+    public  GameWindow() {
+
+        this.setSize(1024, 600);
 
         this.gameCanvas = new GameCanvas();
 
         this.add(this.gameCanvas);//truyen game nay vao windown
 
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    gameCanvas.positionXPlayer -= 8;
+
+                    if (gameCanvas.positionXPlayer <= 0){
+                        gameCanvas.positionXPlayer = 1024;
+                        gameCanvas.positionYPlayer = random.nextInt(601);}
+
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    gameCanvas.positionXPlayer += 8;
+
+                    if (gameCanvas.positionXPlayer >=1024){
+                        gameCanvas.positionXPlayer = 0;
+                        gameCanvas.positionYPlayer = random.nextInt(601);}
+                       }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN){
+                    gameCanvas.positionYPlayer += 8;
+
+                    if (gameCanvas.positionYPlayer >= 600){
+                        gameCanvas.positionYPlayer = 0;
+                        gameCanvas.positionXPlayer = random.nextInt(1025);}
+                    }
+                 if (e.getKeyCode() == KeyEvent.VK_UP){
+                    gameCanvas.positionYPlayer -=8;
+                    if (gameCanvas.positionYPlayer <= 0){
+                        gameCanvas.positionYPlayer = 600;
+                        gameCanvas.positionXPlayer = random.nextInt(1025);}
+                    }
+                }
+
+
+
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(1);
+            }
+        });
+
+
         this.setVisible(true);
     }
 
-}
+
+    public void gameLoop() {
+
+        while (true) {
+            long currentTime = System.nanoTime();//tinh tu 0 gio 0 phut 0 giay ngay 1 thang 1 nam 1970 den thoi diem hien tai
+            if (currentTime - this.lastTime >= 17_000_000) {
+                this.gameCanvas.positionXStar -= 3;//toc do chay cua ngoi sao la 3
+
+                //cho enemy di chuyen cheo
+                this.gameCanvas.positionXEnemy -= 1;
+                this.gameCanvas.positionYEnemy -= 1;
+
+                this.gameCanvas.positionXEnemy += this.gameCanvas.moveXEnemy;
+                this.gameCanvas.positionYEnemy += this.gameCanvas.moveYEnemy;
+
+                if (this.gameCanvas.positionXEnemy >= 1024 || this.gameCanvas.positionXEnemy <=0){
+                    this.gameCanvas.moveXEnemy = -this.gameCanvas.moveXEnemy;
+                }
+                if (this.gameCanvas.positionYEnemy >= 600 || this.gameCanvas.positionYEnemy <= 0){
+                    this.gameCanvas.moveYEnemy = -this.gameCanvas.moveYEnemy;
+                }
+
+                this.gameCanvas.renderAll();
+                //this.gameCanvas.positionXPlayer -= 1; tu dong di chuyen =1
+                this.lastTime = currentTime;
+            }
+
+        }
+
+        //Thread.sleep(20);// dung de nghi
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
